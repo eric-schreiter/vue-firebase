240c71e49da65e5bd1c7984550e15749940432f8
Eric <mail@shter.de>
2019-04-24 07:04:58 +0200

    vue routers and active links

--- routing/src/components/Navbar.vue
+++ routing/src/components/Navbar.vue
@@ -4,6 +4,21 @@
             <li><router-link to="/">Home</router-link></li>
             <li><router-link :to="{name: 'About'}">About</router-link></li>
         </ul>
+        <h2>User Profiles</h2>
+        <ul>
+            <li v-for="(id, index) in userIds" :key="index">
+                <router-link :to="{name: 'ViewProfile', params:{user_id: id }}">
+                    <span>Profile {{ id }}</span>
+                </router-link>
+            </li>
+        </ul>
+        <h2>Navigation Controls</h2>
+        <ul>
+            <li><button @click="goHome">Redirect to Home</button></li>
+            <li><button @click="goBack">Go Back</button></li>
+            <li><button @click="goForward">Go Forward</button></li>
+
+        </ul>
     </nav>
 </template>
 
@@ -12,7 +27,19 @@ export default {
     name:'Navbar',
     data(){
         return {
-
+            userIds: ['1', '2','3','4']
+        }
+    },
+    methods:{
+        // page jumps 
+        goHome(){
+            this.$router.push({name: 'Home'},)
+        },
+        goBack(){
+            this.$router.go(-1)
+        },
+        goForward(){
+            this.$router.go(+1)
         }
     }
 }
@@ -30,5 +57,8 @@ export default {
         color:rebeccapurple;
         font-size:2rem;
         text-decoration: none;
+    }
+    a.router-link-exact-active {
+        color:green;
     }
 </style>
